d6de9613c246d4d3430fb715bf52ead36dcfd37a
ray-a135 <rachelauguste36@gmail.com>
2025-04-18 14:06:01 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "swords":return img`
+. . . . . . . . . . . . . . . . 
+. . b . . . . . . b b . . . . . 
+. . b b . . . . . b b . . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b b . . . . 
+. . b b b . . . . b b . . . . . 
+. . b b . . . . . b b . . . . . 
+. . b b . . . . . b b . . . . . 
+. . b . . . . . . b . . . . . . 
+. e e e e . . . e e e e . . . . 
+. . e e . . . . . e e . . . . . 
+. . e . . . . . . e . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "lava":return img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+`;
+            case "image3":
+            case "fireball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. 2 2 2 2 2 . . . . 2 2 2 2 . . 
+. 2 2 2 2 2 . . . . 2 2 2 2 . . 
+. 2 2 2 2 2 . . . . 2 2 2 2 2 . 
+. 2 2 2 2 2 . . . . 2 2 2 2 2 . 
+. 2 2 2 2 2 . . . . 2 2 2 2 . . 
+. 2 2 2 2 2 . . . . 5 5 5 5 . . 
+. 5 5 5 5 5 . . . . 5 5 5 5 . . 
+. 5 5 5 5 4 . . . . 4 4 4 4 . . 
+. 5 5 4 5 4 . . . . 4 4 4 4 . . 
+. 4 4 4 4 4 . . . . 4 4 4 4 . . 
+. 4 4 4 4 4 . . . . 4 4 . . . . 
+. . . . 4 4 . . . . 4 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "player 2":return img`
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+f f f f f 2 2 2 2 2 2 f f f f f 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
